4d7db4c860ffd00806810900d5cee82cab5ceada
hi.mithun004@students.diu.ac <hi.mithun004@students.diu.ac>
2025-05-08 18:15:11 +0600

    First Functionality

--- script.js
+++ script.js
@@ -4,3 +4,41 @@ const ctx = canvas.getContext('2d');
 const colorPicker = document.getElementById('colorPicker');
 const brushSize = document.getElementById('brushSize');
 const clearBtn = document.getElementById('clearBtn');
+
+// Setting canvas to full size
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight - document.querySelector('.toolbar').offsetHeight;
+}
+resizeCanvas();
+// window.addEventListener('resize', resizeCanvas);
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight -document.querySelector('.toolbar').offsetHeight;
+
+let drawing = false;
+
+canvas.addEventListener('mousedown', ()=> drawing = true);
+canvas.addEventListener('mouseup', ()=> drawing = false);
+canvas.addEventListener('mouseout', ()=> drawing = false);
+
+canvas.addEventListener('mousemove', draw);
+
+function draw(e) {
+    if (!drawing) return;
+
+    ctx.lineWidth = brushSize.value;
+    ctx.lineCap = 'rount';
+    ctx.strokeStyle = colorPicker.value;
+
+    ctx.lineTo(e.clientX, e.clientY-document.querySelector('.toolbar').offsetHeight);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(e.clientX, e.clientY-document.querySelector('.toolbar').offsetHeight);
+
+}
+
+clearBtn.addEventListener('click', () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
