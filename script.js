40ffa9d8c0974b8711d25c62ac3256f46a8f7ce5
hi.mithun004@students.diu.ac <hi.mithun004@students.diu.ac>
2025-05-08 18:05:27 +0600

    Second commit. Nothing ready yet!

---
+++ script.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+const colorPicker = document.getElementById('colorPicker');
+const brushSize = document.getElementById('brushSize');
+const clearBtn = document.getElementById('clearBtn');
